2de1789470189bc7d432a00421e395e9a3f393eb
ajhh <hwangaustinjh@gmail.com>
2018-01-05 18:53:16 -0800

    fixes event time offset

--- tests/spec/contentSpec.js
+++ tests/spec/contentSpec.js
@@ -17,31 +17,31 @@ describe("Course Event Time Functions", function() {
   });
   it("should convert a course start time to api format", function() {
     expect(GetClassStart("9:30 AM-10:45 AM", ["T"], "winter2018"))
-      .toEqual("2018-01-16T09:30:00-07:00");
+      .toEqual("2018-01-16T10:30:00-07:00");
   });
   it("should convert a course end time to api format", function() {
     expect(GetClassEnd("9:30 AM-10:45 AM", ["T"], "winter2018"))
-      .toEqual("2018-01-16T10:45:00-07:00");
+      .toEqual("2018-01-16T11:45:00-07:00");
   });
 });
 
 describe("Exam Event Time Functions", function() {
   it("should convert an exam start time to api format", function() {
     expect(GetExamStart("Friday, January 12, 2018 12:00 PM - 3:00 PM"))
-      .toEqual("2018-01-12T12:00:00-07:00");
+      .toEqual("2018-01-12T13:00:00-07:00");
   });
   it("should convert an exam end time to api format", function() {
     expect(GetExamEnd("Friday, January 12, 2018 12:00 PM - 3:00 PM"))
-      .toEqual("2018-01-12T15:00:00-07:00");
+      .toEqual("2018-01-12T16:00:00-07:00");
   });
 });
 
 
 describe("MilitaryTime function", function() {
   it("should convert a 12 hour time to 24 hour", function () {
-    expect(MilitaryTime(["8:00", "AM"])).toEqual("08:00");
-    expect(MilitaryTime(["12:30", "PM"])).toEqual("12:30");
-    expect(MilitaryTime(["3:30", "PM"])).toEqual("15:30");
-    expect(MilitaryTime(["12:45", "AM"])).toEqual("00:45");
+    expect(MilitaryTime(["8:00", "AM"])).toEqual("09:00");
+    expect(MilitaryTime(["12:30", "PM"])).toEqual("13:30");
+    expect(MilitaryTime(["3:30", "PM"])).toEqual("16:30");
+    expect(MilitaryTime(["12:45", "AM"])).toEqual("01:45");
   });
 });
